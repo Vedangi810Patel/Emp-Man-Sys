44c03af926a3f514c33486873752ec8d4bed4bd7
vedangi patel <vepatel@netclues.lan>
2024-05-21 11:56:56 +0530

    Changes in Log In component

--- front-end/emp-man-sys/src/components/LogIn/LogIn.js
+++ front-end/emp-man-sys/src/components/LogIn/LogIn.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
 const LogIn = () => {
-  return (
-    <div className='login-container d-flex justify-content-center align-items-center vh-100 mt-3 mb-2'>
-        <div className='login-form-container '>
-            <h2> Log In </h2>
-            <form className='login-form'>
-                <div>
-                    <lable htmlFor="email"> <strong> Email : </strong> </lable>
-                    <input type="email" name="email" autoComplete='off' placeholder='Enter Email...' 
-                    className='form-control rounded-0'/>
-                </div>
-                <div>
-                    <lable htmlFor="password"> <strong> Password : </strong> </lable>
-                    <input type="password" name="password" placeholder='Enter Password...' 
-                    className='form-control rounded-0'/>
-                </div>
-                <button className='btn btn-success w-100 rounded-0'> LogIn </button>
-            </form>
+    return (
+        <div className='login-container d-flex justify-content-center align-items-center vh-100 mt-3 mb-2'>
+            <div className='login-form-container '>
+                <h2 className='login'> Log In </h2>
+                <form className='login-form'>
+                    <div className='form-component'>
+                        <lable htmlFor="email"> <strong> Email : </strong> </lable>
+                        <input type="email" name="email" autoComplete='off' placeholder='Enter Email...'
+                            className='form-control rounded-0' />
+                    </div>
+                    <div className='form-component'>
+                        <lable htmlFor="password"> <strong> Password : </strong> </lable>
+                        <input type="password" name="password" placeholder='Enter Password...'
+                            className='form-control rounded-0' />
+                    </div>
+                    <div className='form-component'>
+                        <button className='btn btn-success w-100 rounded-0 '> LogIn </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default LogIn
